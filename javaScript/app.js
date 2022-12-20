3059870f1a09cdfd63067d4a42ceda35c17e41be
franklyn-ezeugonna <franklynezeugonna@gmail.com>
2022-12-19 20:48:31 -0800

    updated the website

--- javaScript/app.js
+++ javaScript/app.js
@@ -1,6 +1,9 @@
 // declaration of variable and getting element
 const navToggle = document.querySelector(`.nav-toggle`);
-const navLinks = document.querySelectorAll(`.nav__link`)
+const navLinks = document.querySelectorAll(`.nav__link`);
+const projects = document.getElementsByClassName('project');
+const developerBtn = document.getElementById('developer-btn');
+const devWorksContainer = document.getElementById('my-work');
 
 // Toggling/creating the Nav button(page) after clicking the hamburger
 navToggle.addEventListener(`click` , ()=>{
@@ -15,3 +18,19 @@ navLinks.forEach((link)=>{
     })
 })
 
+//Developer btn is clicked
+developerBtn.addEventListener('click', () => {
+    developerBtn.classList.add('colored-btn');
+    devWorksContainer.style.display = 'grid';
+})
+
+//Hover state for projects
+for(let project of projects){
+    //Check if the developer btn is active
+    project.addEventListener('mouseover', () => {
+        if(developerBtn.classList.contains('colored-btn')) project.lastElementChild.style.display = 'flex';
+    });
+    project.addEventListener('mouseout', () => {
+        if(developerBtn.classList.contains('colored-btn')) project.lastElementChild.style.display = 'none';
+    });
+}
